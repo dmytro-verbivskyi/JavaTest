091a91d9f1fed7324a79565194675eaaf970c986
Dima Home <dverbovskiy@gmail.com>
2013-06-26 00:23:12 +0300

    наследование и переопределение property ver2

--- src/main/java/JavaTest/Main.java
+++ src/main/java/JavaTest/Main.java
@@ -1,19 +1,25 @@
 package JavaTest;
 
 class Base {
-    public static int static_method()   { return 1; }
-    public        int instance_method() { return 2; }
+    static int static_method()   { return 1; }
+           int instance_method() { return 2; }
 
-    public static int static_property = 5;
-    public        int instance_property = 6;
+    static int static_property   = 5;
+           int instance_property = 6;
+
+    static String static_ref_property   = "9";
+           String instance_ref_property = "10";
 }
 
 public class Main extends Base {
-    public static int static_method()   { return 3; }
-    public        int instance_method() { return 4; }
+    static int static_method()   { return 3; }
+           int instance_method() { return 4; }
 
-    public static int static_property = 7;
-    public        int instance_property = 8;
+    static int static_property   = 7;
+           int instance_property = 8;
+
+    static String static_ref_property   = "11";
+           String instance_ref_property = "12";
 
     public static void main(String[] args) {
         Base b = new Main();
@@ -24,5 +30,8 @@ public class Main extends Base {
 
         System.out.println(b.static_property   + ", " + s.static_property);
         System.out.println(b.instance_property + ", " + s.instance_property);
+
+        System.out.println(b.static_ref_property   + ", " + s.static_ref_property);
+        System.out.println(b.instance_ref_property + ", " + s.instance_ref_property);
     }
 }
